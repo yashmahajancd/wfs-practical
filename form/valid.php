28068fc4806e4b9b2fafdf2ef5ea6056a720a941
Yash Mahajan <mahajanyash2185@gmail.com>
2026-08-18 04:36:10 +0530

    add form handling

--- form/valid.php
+++ form/valid.php
@@ -24,6 +24,24 @@ $dob = $_POST['date'];
 $contact = $_POST['contact'];
 $email = $_POST['email'];
 
+////////////////////////////////////
 
+if(empty($name))
+{
+    echo "Name can't be empty! <br>";
+}
+elseif(!preg_match('/^[a-z]*[a-z]$/', $name))
+{
+    echo "Name must be in small letter! <br>";
+}
+
+if(empty($dob))
+{
+    echo "DOB can't be empty! <br>";
+}
+elseif(!preg_match('/^(0[1-9]|[12][0-9]|3[01])[-\/\.](0[1-9]|1[012])[-\/\.](19|20)[0-9]{2}$/', $dob))
+{
+    echo "Date must be in DD-MM-YYYY format! <br>";
+}
 
 ?>
